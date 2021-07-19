052f191274a539d96f38855322aa118dc9fcf0b7
correctionjs1-3 <tana39754@gmail.com>
2021-07-19 13:25:33 +0900

    訂正完了しました

--- app1-4.js
+++ app1-4.js
@@ -1,32 +1,34 @@
-console.log("=======================");
-console.log("現在持っているタスク一覧");
-console.log("=======================");
-
-const addtask = [
-    '[内容]机を片付ける、[ジャンル]掃除',
-    '[内容]牛乳を買う、[ジャンル]買い物',
-    '[内容]寝る、[ジャンル]睡眠'
-]
-addtask.forEach((task, index) => {
-    console.log(`${index} : ${task}`);
-});
-const addtaskcord = prompt("タスクを入力してください");
-const addtask12 = prompt("ジャンルを入力してください");
-alert("新しいタスクが入力されました");
-console.log("======================");
-console.log("現在持っているタスク一覧");
-console.log("======================");
-
-const task = {};
-const {
-    type,
-    content} = task;
-const tasks = [];
-tasks.push(task);
-addtask.push(addtaskcord, addtask12);
-tasks.forEach((i,tasks) => {
-    for(i=0; i<3; i++){
-        console.log(`${i} : ${addtask[i]}`)
-    }console.log(`${i} : [内容] ${addtaskcord}、 [ジャンル] ${addtask12}、`)
-});
-prompt('「確認　追加　削除　終了」の4ついづれかを入力してください');
+'use strict';
+const title =
+'======================\n現在持っているタスク一覧\n======================';
+const tasks = [
+    {
+        content: '机を片付ける',
+        genre: '掃除'
+    },
+    {
+        content: '牛乳を買う',
+        genre: '買い物'
+    },
+    {
+        content: '散歩する',
+        genre: '運動'
+    },
+];
+const pushTask = (job, type) =>
+tasks.push({ content: job, genre: type});
+const taskLists = () => {
+    console.log(title);
+    let index = 0;
+    tasks.forEach(task => {
+        console.log(`${index} : [内容] ${task.content}
+        [ジャンル] ${task.genre}`);
+        index++;
+    });
+}
+taskLists();
+let job = prompt('タスクを入力してください。');
+let type = prompt('ジャンルを入力してください');
+pushTask(job, type);
+alert('新しいタスクを入力しました');
+taskLists();
